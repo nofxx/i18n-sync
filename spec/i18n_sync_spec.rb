b2f564e754300b817a19ed34c4661f20033d0b04
Marcos Piccinini <x@nofxx.com>
2010-07-16 10:41:56 -0300

    fixes my "integration" specs.. need a better way to handle the gem loading when calling the executable

--- spec/i18n_sync_spec.rb
+++ spec/i18n_sync_spec.rb
@@ -3,6 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 EN = "spec/work/en.yml"
 
+def run(comm)
+  `bin/i18s #{comm}`
+end
+
 describe "I18nSync" do
   before do
     `rm -rf spec/work`
@@ -12,27 +16,27 @@ describe "I18nSync" do
   it "should create a new one" do
     newfile = "spec/work/fo.yml"
     `rm #{newfile}` if File.exists?(newfile)
-    `bin/i18s #{EN} fo`
+    run("#{EN} fo")
     File.read(newfile).should eql("\nfo: \n  sync: To Sync It!\n  test: Test\n")
   end
 
   it "should sync files nicely" do
-    `bin/i18s spec/work/pt.yml`
+    run("spec/work/pt.yml")
     File.read(EN).should eql("\nen: \n  new: \"Uau, isso é novo\"\n  sync: To Sync It!\n  test: Test\n")
   end
 
   it "should respect namespaces" do
-    `bin/i18s spec/work/pt.yml`
+    run("spec/work/pt.yml")
     File.read("spec/work/named.pt.yml").should eql("\npt:\n  something: Algo\n")
   end
 
   it "should respect namespaces" do
-    `bin/i18s spec/work/named.en.yml`
+    run("spec/work/named.en.yml")
     File.read("spec/work/named.pt.yml").should eql("\npt: \n  another: Another\n  something: Algo\n")
   end
 
   it "should order alphabeticaly namespaces" do
-    `bin/i18s spec/work/order.pt.yml`
+    run("spec/work/order.pt.yml")
     File.read("spec/work/order.en.yml").should eql("\nen: \n  alpha: Alpha\n  blue: Blue\n  zebra: Zebra\n")
     File.read("spec/work/named.pt.yml").should eql("\npt:\n  something: Algo\n")
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-TEST = true
 require 'i18n_sync'
 begin
   require 'spec'
