5f06f28a170f1a2be19fb25630b1d9a6a05f43ce
Marcos Piccinini <x@nofxx.com>
2011-04-04 18:14:02 -0300

    change implementation looks like it works

--- lib/i18n_sync.rb
+++ lib/i18n_sync.rb
@@ -1,96 +1,101 @@
 #
 # Based on translation rake task on spree (http://spreecommerce.com/)
 #
+require 'ya2yaml'
+
 class I18S
 
-  def initialize(argv, opts, argf=[])
+  def initialize(argv, opts = {}, argf=[])
     # argf.each { |file| p file }
-    @fullpath, *new_ones = argv
+    @fullpath, *@new_ones = argv
     @file, *path = @fullpath.split("/").reverse # hm.. hack,,, in 1.9
-    @path = path.reverse.join("/")              # can splat the first
-    @path ||= "."
-    s = @file.split(".")
-    @namespace =  s.length > 2 ? s[0]  : false
-    @lang = s[-2]
+    @path = path.reverse.join("/") || "."       # can splat the first
+    _, @lang, @namespace = @file.split(".").reverse
     @debug = opts[:trace]
     @order = opts[:order]
+    @comments, @words = read_file(@fullpath, @lang)
+  end
+
+  def work
     puts "Start work on #{@file} (#{@lang})"
-    @words = get_translation_keys
-    if new_ones.empty?
-      sync
-    else
-      new_ones.each do |file|
-        puts "Creating new file #{file}"
-        create(file)
-      end
+    @new_ones.empty? ? sync : create_new_files
+  end
+
+  def create_new_files
+    @new_ones.each do |file|
+      puts "Creating new file #{file}"
+      create(file)
     end
   end
 
   def sync
     Dir["#{@path}/*.{yml,rb}"].each do |filename|
       #next if filename.match('_rails')
-      basename = File.basename(filename, '.yml')
-      if basename =~ /\./
-        n, basename = basename.split(".")
-        next unless @namespace && @namespace == n
+      next if filename =~ /#{@file}/
+      lang, namespace = File.basename(filename, '.yml').split(".").reverse  #filename.split("/").last.split(".").reverse
+      if namespace
+        next unless @namespace && @namespace == namespace
+      else
+        next if @namespace
       end
-      puts "Writing #{filename} -> #{basename}"
-      (comments, other) = read_file(filename, basename)
-      @words.each { |k,v| other[k] ||= @words[k] }                    #Initializing hash variable as empty if it does not exist
-      other.delete_if { |k,v| !@words[k] }                            #Remove if not defined in en-US.yml
-      write_file(filename, basename, comments, other)
+
+      puts "Writing #{filename}"
+      (_comments, other) = read_file(filename, lang)
+      # Initializing hash variable as empty if it does not exist
+      @words.each { |k,v| other[k] ||= @words[k] }
+      # Remove if not defined in master
+      other.delete_if { |k,v| !@words[k] }
+      write_file(filename, lang, @comments, other)
     end
   end
 
   def create(file)
     fullpath = file =~ /\// ? file : "#{@path}/#{file}.yml"
     return puts("File exists.") if File.exists?(fullpath)
-    write_file(fullpath, file, "", @words)
-  end
-
-  #Retrieve US word set
-  def get_translation_keys
-    (dummy_comments, words) = read_file(@fullpath, @lang)
-    words
+    write_file(fullpath, file, @comments, @words)
   end
 
   #Retrieve comments, translation data in hash form
   def read_file(filename, basename)
-    (comments, data) = IO.read(filename).split(/\n*#{basename}:\s*\n/)   #Add error checking for failed file read?
-    return comments, create_hash(data, basename)
+    comments = File.read(filename).each_line.select { |l| l =~ /^\w*#/}.join("\n")
+    [comments, YAML.load(File.open(filename, "r:utf-8"))[basename]]
   end
 
   #Creates hash of translation data
-  def create_hash(data, basename)
-    words = Hash.new
-    return words if !data
-    parent = Array.new
-    previous_key = 'base'
-    data.split("\n").each do |w|
-      next if w.strip.empty?
-      (key, value) = w.split(':')
-      value ||= ''
-      shift = (key =~ /\w/)/2 - parent.size                             #Determine level of current key in comparison to parent array
-      key = key.sub(/^\s+/,'')
-      parent << previous_key if shift > 0                               #If key is child of previous key, add previous key as parent
-      (shift*-1).times { parent.pop } if shift < 0                      #If key is not related to previous key, remove parent keys
-      previous_key = key                                                #Track key in case next key is child of this key
-      words[parent.join(':')+':'+key] = value
-    end
-    words
-  end
+  #def create_hash(data, filename)
+    # return {} unless data
+    # words = Hash.new
+    # return words if !data
+    # parent = Array.new
+    # previous_key = 'base'
+    # data.split("\n").each do |w|
+    #   next if w.strip.empty?
+    #   w.sub!(":", "") if w =~ /^(\s*)\:/
+    #   (key, *value) = w.split(':')
+    #   value = value.join# ||= ''
+    #   shift = (key =~ /\w/)/2 - parent.size                             #Determine level of current key in comparison to parent array
+    #   key = key.sub(/^\s+/,'')
+    #   parent << previous_key if shift > 0                               #If key is child of previous key, add previous key as parent
+    #   (shift*-1).times { parent.pop } if shift < 0                      #If key is not related to previous key, remove parent keys
+    #   previous_key = key                                                #Track key in case next key is child of this key
+    #   words[parent.join(':')+':'+key] = value
+    # end
+    # words
+  #end
 
   #Writes to file from translation data hash structure
-  def write_file(filename,basename,comments,words)
-    File.open(filename, "w") do |log|
-      log.puts(comments+"\n"+basename+": \n")
-      words.sort.each do |k,v|
-        keys = k.split(':')
-        (keys.size-1).times { keys[keys.size-1] = '  ' + keys[keys.size-1] }   #Add indentation for children keys
-        log.puts(keys[keys.size-1]+':'+v+"\n")
-      end
+  def write_file(filename, basename, comments, data)
+    File.delete filename if File.exists? filename
+    File.open(filename, "w:utf-8") do |y|
+      y.puts(comments) if comments
+      #y.puts(basename + ":\n")
+      y.puts({ basename => data }.ya2yaml )# (:sick_compatible => true))
+      # words.sort.each do |k,v|
+      #   keys = k.split(':')
+      #   (keys.size-1).times { keys[keys.size-1] = '  ' + keys[keys.size-1] }   #Add indentation for children keys
+      #   y.puts(keys[keys.size-1]+':'+v+"\n")
+      # end
     end
-
   end
 
   def out(txt)
--- spec/i18n_sync_spec.rb
+++ spec/i18n_sync_spec.rb
@@ -8,36 +8,89 @@ def run(comm)
 end
 
 describe "I18nSync" do
-  before do
+  before(:each) do
     `rm -rf spec/work`
     `cp -rf spec/fixtures spec/work`
   end
 
-  it "should create a new one" do
-    newfile = "spec/work/fo.yml"
-    `rm #{newfile}` if File.exists?(newfile)
-    run("#{EN} fo")
-    File.read(newfile).should eql("\nfo: \n  sync: To Sync It!\n  test: Test\n")
+  describe "Unit" do
+
+    let(:i) { I18S.new("spec/work/en.yml") }
+
+    it "should parse master file" do
+      i.instance_variable_get("@path").should eql("spec/work")
+      i.instance_variable_get("@lang").should eql("en")
+      i.instance_variable_get("@file").should eql("en.yml")
+    end
+
+    it "should read comments" do
+      i.instance_variable_get("@comments").should eql("# Comment cool\n")
+    end
+
+    it "should read the hash" do
+      i.instance_variable_get("@words").should eql({"sync"=>"To Sync It!", "test"=>"Test"})
+    end
+
+    it "should work fine with other files" do
+      i2 = I18S.new("spec/work/pt.yml")
+      i2.instance_variable_get("@lang").should eql("pt")
+      i2.instance_variable_get("@comments").should be_empty
+    end
+
   end
 
-  it "should sync files nicely" do
-    run("spec/work/pt.yml")
-    File.read(EN).should eql("\nen: \n  new: \"Uau, isso é novo\"\n  sync: To Sync It!\n  test: Test\n")
+  describe "Namespaced" do
+    let(:i) { I18S.new("spec/work/named.en.yml") }
+
+    it "should parse master file" do
+      i.instance_variable_get("@path").should eql("spec/work")
+      i.instance_variable_get("@lang").should eql("en")
+      i.instance_variable_get("@namespace").should eql("named")
+      i.instance_variable_get("@file").should eql("named.en.yml")
+    end
+
+    it "should read the hash" do
+      i.instance_variable_get("@words").should eql({"something"=>"Something", "another"=>"Another"})
+    end
+
   end
 
-  it "should respect namespaces" do
-    run("spec/work/pt.yml")
-    File.read("spec/work/named.pt.yml").should eql("\npt:\n  something: Algo\n")
+  describe "Acceptance" do
+
+    it "should create a new one" do
+      newfile = "spec/work/fo.yml"
+      `rm #{newfile}` if File.exists?(newfile)
+      run("#{EN} fo")
+      File.read(newfile).should eql("# Comment cool\n--- \nfo: \n  sync: \"To Sync It!\"\n  test: Test\n")
+    end
+
+    it "should sync files nicely" do
+      run("spec/work/pt.yml")
+      File.read(EN).should eql("\n--- \nen: \n  new: \"Uau, isso é novo\"\n  sync: \"To Sync It!\"\n  test: Test\n")
+    end
+
+    it "should respect namespaces" do
+      run("spec/work/pt.yml")
+      File.read("spec/work/named.pt.yml").should eql("\npt:\n  something: Algo\n")
+    end
+
+    it "should work with namespaces" do
+      run("spec/work/named.en.yml")
+      File.read("spec/work/named.pt.yml").should eql("\n--- \npt: \n  another: Another\n  something: Algo\n")
+      File.read("spec/work/pt.yml").should eql("\npt:\n  sync: To Sync It!\n  test: Teste\n  new: \"Uau, isso é novo\"\n")
+    end
+
+    it "should order alphabeticaly namespaces" do
+      run("spec/work/order.pt.yml")
+      File.read("spec/work/order.en.yml").should eql("\n--- \nen: \n  alpha: Alpha\n  blue: Blue\n  zebra: Zebra\n")
+      File.read("spec/work/named.pt.yml").should eql("\npt:\n  something: Algo\n")
+    end
+
+    it "should work with extra chars" do
+      run("spec/work/extra.en.yml")
+      File.read("spec/work/extra.pt.yml").should eql("\n--- \npt: \n  normal: Normal\n  with_colon: \"Value: Rock\"\n  yup: Sim\n")
+    end
+
   end
 
-  it "should respect namespaces" do
-    run("spec/work/named.en.yml")
-    File.read("spec/work/named.pt.yml").should eql("\npt: \n  another: Another\n  something: Algo\n")
-  end
-
-  it "should order alphabeticaly namespaces" do
-    run("spec/work/order.pt.yml")
-    File.read("spec/work/order.en.yml").should eql("\nen: \n  alpha: Alpha\n  blue: Blue\n  zebra: Zebra\n")
-    File.read("spec/work/named.pt.yml").should eql("\npt:\n  something: Algo\n")
-  end
 end
